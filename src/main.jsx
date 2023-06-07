8f3d9052a5a5e199febe05b2bedb7ab4a7d504a4
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-06-07 22:54:15 +0600

    navber ready

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
-// import Homepage from './pages/HomePage/Homepage';
+import Homepage from './pages/HomePage/Homepage';
 // import ErrorPage from './pages/ErrorPage/ErrorPage';
 // import Blogs from './pages/Blogs/Blogs';
 // import LoginPage from './pages/LoginPage/LoginPage';
