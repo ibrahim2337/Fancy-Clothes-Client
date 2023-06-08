4e04b04e0f4ab4517cb6ded56bd9be1bc55fd1c0
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-06-08 20:49:41 +0600

    update section ready

--- src/components/Update/Update.jsx
+++ src/components/Update/Update.jsx
@@ -5,11 +5,9 @@ const Update = () => {
         <div className="container mx-auto flex flex-col justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:space-x-12 lg:justify-between lg:flex-row">
           <div className="flex flex-col space-y-4 text-center lg:text-left">
             <h1 className="text-5xl font-bold leading-none">
-            Dear - Stay tune and get the latest update
+              Dear - Stay tune and get the latest update
             </h1>
-            <p className="text-lg">
-            Far far away, behind the word mountains
-            </p>
+            <p className="text-lg">Far far away, behind the word mountains</p>
           </div>
           <div className="flex flex-row items-center self-center justify-center flex-shrink-0 shadow-md lg:justify-end ">
             <div className="flex flex-row ">
