7b78481db5acbe2496eafb2d4fd3e4ab070fa72d
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-06-12 23:56:44 +0600

    classes section ready

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,15 +1,15 @@
 const Gallery = () => {
   return (
     <div className="pt-24">
-        <div className="max-w-2xl mt-10  mx-auto mb-16 text-center">
-            <p className="text-xl font-medium text-center text-[#1EB2A6] ">
+        <div className="max-w-2xl mt-10  mx-auto  text-center">
+            <p className="text-xl font-bold text-center text-[#1EB2A6] ">
               OUR GALLERY
             </p>
             <h2 className="text-4xl font-bold mt-3">
               HELLO! Welcome to Photo Gallery With Creative & Unique Style
             </h2>
           </div>
-      <section className="">
+      <section className="mt-10">
         <div className="container flex flex-col justify-center p-4 mx-auto">
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
             <img
