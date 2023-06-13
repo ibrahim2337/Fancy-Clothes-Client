16ceac3378bea2773f97acebc46e73ceb10a8a1f
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-06-13 16:07:17 +0600

    gallery pic add

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,32 +1,158 @@
 const Gallery = () => {
   return (
     <div className="pt-24">
-        <div className="max-w-2xl mt-10  mx-auto  text-center">
-            <p className="text-xl font-bold text-center text-[#1EB2A6] ">
-              OUR GALLERY
-            </p>
-            <h2 className="text-4xl font-bold mt-3">
-              HELLO! Welcome to Photo Gallery With Creative & Unique Style
-            </h2>
-          </div>
+      <div className="max-w-2xl mt-10  mx-auto  text-center">
+        <p className="text-xl font-bold text-center text-[#1EB2A6] ">
+          OUR GALLERY
+        </p>
+        <h2 className="text-4xl font-bold mt-3">
+          HELLO! Welcome to Photo Gallery With Creative & Unique Style
+        </h2>
+      </div>
       <section className="mt-10">
-        <div className="container flex flex-col justify-center p-4 mx-auto">
-          <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2">
+        <div className="container flex flex-col justify-center px-5 lg:px-24 mx-auto">
+          <div className="grid grid-cols-1 gap-10 lg:grid-cols-4 sm:grid-cols-2">
             <img
-              className="object-cover w-full dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/300x300/?1"
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/DYNJK12/pexels-photo-1857375.jpg"
             />
             <img
-              className="object-cover w-full dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/300x300/?2"
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/t3Ndt8P/pexels-photo-2755612.jpg"
             />
             <img
-              className="object-cover w-full dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/300x300/?3"
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/TtnDHb9/pexels-photo-2737004.jpg"
             />
             <img
-              className="object-cover w-full dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/300x300/?4"
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/F6hCy6j/pexels-photo-2681751.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/WVNxQkx/pexels-photo-2584269.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/9868hrD/pexels-photo-2829373.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/txSTCQn/pexels-photo-2034435.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/ZfsPCjq/wedding-dresses-fashion-character-bride-157757.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="
+              https://i.ibb.co/HdLJzfv/pexels-photo-3769148.jpg
+              "
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/xmcwTjk/pexels-photo-7453194.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/frYMPCy/pexels-photo-884979.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/gSKrwKk/pexels-photo-904117.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/WxLn5mV/pexels-photo-983569.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/Fgfm1tv/pexels-photo-1187954.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500  aspect-square"
+              src="https://i.ibb.co/VSsZwS0/pexels-photo-3014853.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/LdJj98p/pexels-photo-1308885.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/kcKT2cB/pexels-photo-5559986.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/HPnRVFj/pexels-photo-9147254.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/q9C9Bfp/pexels-photo-7788328.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/3B6yphy/pexels-photo-7169749.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/crhGXrx/pexels-photo-7139956.webp"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src=" https://i.ibb.co/zmxN5kf/pexels-photo-7139777.webp"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src=" https://i.ibb.co/JRHHpqp/pexels-photo-6623811.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src=" https://i.ibb.co/sRm9jDy/pexels-photo-5560019.webp"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/hLJkbM8/pexels-photo-5559990.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src=" https://i.ibb.co/3Rtf41m/pexels-photo-3771506.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/D5sT03j/pexels-photo-3662975.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/phQsZs3/pexels-photo-3661550.webp"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/mDgwXYp/pexels-photo-3614523.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/Z6fy97j/pexels-photo-1765423.jpg"
+            />
+            <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src="https://i.ibb.co/Gxk24ZP/pexels-photo-1620788.jpg"
+            />
+             <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src="https://i.ibb.co/dL82k9x/pexels-photo-1416736.jpg"
+            />
+             <img
+              className="object-cover w-full dark:bg-gray-500 rounded-full aspect-square"
+              src=""
+            />
+             <img
+              className="object-cover w-full dark:bg-gray-500 rounded-2xl aspect-square"
+              src=""
+            />
+             <img
+              className="object-cover w-full dark:bg-gray-500  rounded-full aspect-square"
+              src=""
             />
           </div>
         </div>
