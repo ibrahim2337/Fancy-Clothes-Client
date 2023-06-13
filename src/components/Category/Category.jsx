c2d727814f33c25d60fd94325647d9b4875e3225
ibrahim2337 <ibrahimkhalilullah680@gmail.com>
2023-06-13 15:30:24 +0600

    project update

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -3,9 +3,7 @@ const Category = () => {
     <div className="mt-20 flex justify-center">
       <div className=" grid lg:grid-cols-3 gap-10">
         <div className=" border rounded-md px-10 py-10 bg-[#1EB2A6] text-white">
-          <div className="">
-            <img src="" alt="" />
-          </div>
+          
           <div className="text-xl text-center mb-2">Downloads</div>
           <div className="text-3xl  py-5 border rounded-md mb-2 border-[#1EB2A6] text-center bg-[#3bbeb4]">
             31K
@@ -14,9 +12,7 @@ const Category = () => {
         </div>
 
         <div className=" border rounded-md px-10 py-10 bg-[#1EB2A6] text-white">
-          <div className="">
-            <img src="" alt="" />
-          </div>
+          
           <div className="text-xl text-center mb-2">New Users</div>
           <div className="text-3xl  py-5 border rounded-md mb-2 border-[#1EB2A6] text-center bg-[#3bbeb4]">
             4,200
@@ -25,9 +21,7 @@ const Category = () => {
         </div>
 
         <div className=" border rounded-md px-10 py-10 bg-[#1EB2A6] text-white">
-          <div className="stat-figure text-secondary">
-            <img src="" alt="" />
-          </div>
+         
           <div className="text-xl text-center mb-2">New Registers</div>
           <div className="text-3xl  py-5 border rounded-md mb-2 border-[#1EB2A6] text-center bg-[#3bbeb4]">
             1,200
